611a9028ff0cb4c714d6e9be80be92559c268d23
ankit-mogha <ankit.mogha@nashtechglobal.com>
2023-08-26 01:02:30 +0530

    Updated services name

---
+++ PaymentService/src/main/java/com/nashtech/payment/PaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nashtech.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(com.nashtech.payment.PaymentServiceApplication.class, args);
+	}
+
+}

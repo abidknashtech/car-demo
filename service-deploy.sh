aed06b995e79b86e637435a1e1d579d9e6aee379
kundan59 <kundangupta59@gmail.com>
2023-09-10 13:25:23 +0530

    update deploy script11

--- service-deploy.sh
+++ service-deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 PROJECT_ID="$(gcloud config get-value project)"
-echo "$1"
 for project in $(cat projects-deploy.txt)
 do
    :
@@ -18,9 +17,9 @@ do
      cd order-service || continue
      mvn clean install || continue
      echo "---------packaging done, start docker build-----------"
-     sudo docker build -f Dockerfile --tag us.gcr.io/$PROJECT_ID/orderservice . || continue
+     sudo docker build -f Dockerfile --t us.gcr.io/"$PROJECT_ID"/orderservice . || continue
      echo  "--------docker build done, docker push---------------"
-     sudo docker push us.gcr.io/$PROJECT_ID/orderservice
+     sudo docker push us.gcr.io/"$PROJECT_ID"/orderservice
      echo  "--------pushed docker image--------------------------"
 
   esac

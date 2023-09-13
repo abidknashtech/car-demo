f72335fb97b71fdbc763472121e07a7d0fa06156
DeepaMittal <deepamittalmca@gmail.com>
2023-09-13 13:41:45 +0530

    resolved comments

--- cart-service/src/main/java/com/nashtech/car/cart/audit/Auditable.java
+++ cart-service/src/main/java/com/nashtech/car/cart/audit/Auditable.java
@@ -1,10 +1,7 @@
 package com.nashtech.car.cart.audit;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.data.annotation.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.util.Date;

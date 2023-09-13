2a6a24ff727e26b70ed95f734f2df11336337a3c
kundan59 <kundangupta59@gmail.com>
2023-09-13 20:37:57 +0530

    update extenal-secret gcsm

--- deployment/gcpresources/terraform/external-secret-gcsm.sh
+++ deployment/gcpresources/terraform/external-secret-gcsm.sh
@@ -2,8 +2,11 @@
 
 GKE_CLUSTER="$1"
 REGION="$2"
+# Authenticate car-demo-gke
 gcloud container clusters get-credentials "$GKE_CLUSTER" --region "$REGION"
+# Create gcpsm-secret with credential
 kubectl create secret generic gcpsm-secret --from-file=secret-access-credentials=key.json
-
+# Add external-secret using helm chart
 helm repo add external-secrets https://charts.external-secrets.io
+# install external-secrets/external-secrets CRD
 helm install external-secrets external-secrets/external-secrets -n default  --set installCRDs=true

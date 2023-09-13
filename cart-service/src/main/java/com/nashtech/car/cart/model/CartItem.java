7886e70e6619ce6dba8806afc6b60700eeb09d39
DeepaMittal <deepamittalmca@gmail.com>
2023-09-13 14:02:18 +0530

    fixed docker file changes

--- cart-service/src/main/java/com/nashtech/car/cart/model/CartItem.java
+++ cart-service/src/main/java/com/nashtech/car/cart/model/CartItem.java
@@ -2,8 +2,7 @@ package com.nashtech.car.cart.model;
 
 import com.nashtech.car.cart.audit.Auditable;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.io.Serializable;
 

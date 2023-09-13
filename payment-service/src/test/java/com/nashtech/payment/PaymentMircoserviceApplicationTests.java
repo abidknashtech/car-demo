b447b1663e596edbc15da702225ee1d26a0d5be5
kundan59 <kundangupta59@gmail.com>
2023-09-13 20:06:58 +0530

    removed test cases

--- payment-service/src/test/java/com/nashtech/payment/PaymentMircoserviceApplicationTests.java
+++ payment-service/src/test/java/com/nashtech/payment/PaymentMircoserviceApplicationTests.java
@@ -6,8 +6,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PaymentMircoserviceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
--- shipment-service/src/test/java/com/nashtech/shipment/ShipmentMircoserviceApplicationTests.java
+++ shipment-service/src/test/java/com/nashtech/shipment/ShipmentMircoserviceApplicationTests.java
@@ -6,8 +6,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ShipmentMircoserviceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
 }

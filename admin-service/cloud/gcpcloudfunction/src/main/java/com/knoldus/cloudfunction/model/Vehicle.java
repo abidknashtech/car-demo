2e0c9479c851f2ae8e3713bb5041f4c58f6b68d5
ankit-mogha <ankit.mogha@nashtechglobal.com>
2023-09-20 15:39:51 +0530

    Add gcp cloud function

---
+++ admin-service/cloud/gcpcloudfunction/src/main/java/com/knoldus/cloudfunction/model/Vehicle.java
@@ -0,0 +1,45 @@
+package com.knoldus.cloudfunction.model;
+
+import lombok.Data;
+
+/**
+ * Represents a Vehicle.
+ */
+@Data
+public class Vehicle {
+    /**
+     * The unique identifier of the car.
+     */
+    private Integer carId;
+
+    /**
+     * The model of the car.
+     */
+    private String model;
+
+    /**
+     * The brand of the car.
+     */
+    private String brand;
+
+    /**
+     * The manufacturing year of the car.
+     */
+    private Integer year;
+
+    /**
+     * The color of the car.
+     */
+    private String color;
+
+    /**
+     * The mileage of the car.
+     */
+    private Double mileage;
+
+    /**
+     * The price of the car.
+     */
+    private Double price;
+
+}

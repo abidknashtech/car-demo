ccfb8d6d45186420306872f7800d969c37d4f260
Vimal Kumar <102946997+vimal-knoldus@users.noreply.github.com>
2023-09-23 21:15:06 +0530

    Update az-svc-deployment.sh

--- az-svc-deployment.sh
+++ az-svc-deployment.sh
@@ -5,17 +5,17 @@ SERVICE_NAME="admin-service"
 DEPLOYMENT_NAME="admin-service"
 RESOURCE_GROUP_NAME="az-nashtech-resource-group"
 
-az aks install-cli
+sudo az aks install-cli
 echo "---------build and deploy $SERVICE_NAME-----------"
 cd "$SERVICE_NAME" || exit
 mvn clean install
 echo "---------packaging done, start docker build-----------"
-docker build -f Dockerfile --tag gcr.io/"$SERVICE_NAME":"$GITHUB_SHA" .
+sudo docker build -f Dockerfile --tag gcr.io/"$SERVICE_NAME":"$GITHUB_SHA" .
 echo  "--------docker build done, docker push---------------"
-docker push gcr.io/"$SERVICE_NAME":"$GITHUB_SHA"
+sudo docker push gcr.io/"$SERVICE_NAME":"$GITHUB_SHA"
 echo  "--------pushed docker image, deploy to aks cluster--------------------------"
 
-az aks get-credentials --resource-group "$RESOURCE_GROUP_NAME" --name "$AKS_CLUSTER"
+sudo az aks get-credentials --resource-group "$RESOURCE_GROUP_NAME" --name "$AKS_CLUSTER"
 # setup kustomize
 curl -sfLo kustomize https://github.com/kubernetes-sigs/kustomize/releases/download/v3.1.0/kustomize_3.1.0_linux_amd64
 chmod u+x ./kustomize

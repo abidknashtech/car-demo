481a5c3eb69a01084d8edbdd0b12d11dcf03ae64
vimal-knoldus <vimal.kumar@knoldus.com>
2023-09-25 17:53:43 +0530

    Update azure deployment file

--- az-svc-deployment.sh
+++ az-svc-deployment.sh
@@ -29,8 +29,6 @@ echo  "---------------set docker image for kustomize-------------------"
 ./kustomize edit set image ntdemocontainerregistry.azurecr.io/IMAGE:TAG=ntdemocontainerregistry.azurecr.io/"$SERVICE_NAME":"$GITHUB_SHA"
 echo "---------------deploy through kubectl------------------"
 # deploy through kubectl
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 ./kustomize build . | kubectl apply -f -
 kubectl rollout status deployment/"$DEPLOYMENT_NAME"

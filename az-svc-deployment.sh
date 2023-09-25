853c7a98fd686fdf3bc4dfdf7888a336e1431c8b
vimal-knoldus <vimal.kumar@knoldus.com>
2023-09-25 13:45:34 +0530

    Update azure deployment file

--- az-svc-deployment.sh
+++ az-svc-deployment.sh
@@ -6,7 +6,7 @@ DEPLOYMENT_NAME="admin-service"
 RESOURCE_GROUP_NAME="az-nashtech-resource-group"
 
 sudo az aks install-cli
-sudo az acr login --name ntdemocontainerregistry --username ntdemocontainerregistry --password rsLWRGZx28OcmFaJPqhykv2RPS6oq4YNLsmlUIcanJ+ACRDRyyRH
+sudo az acr login --name ntdemocontainerregistry --username ntdemocontainerregistry --password BjGQNe5mzQquXBcYe6rh6A5aAMhDtUY5x5zs8TCzPX+ACRCQa/EP
 echo "---------build and deploy $SERVICE_NAME-----------"
 cd "$SERVICE_NAME" || exit
 mvn clean install

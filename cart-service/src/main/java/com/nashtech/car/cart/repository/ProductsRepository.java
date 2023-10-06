ada82e017e57fe35154c52945be531966f0a0141
abidknashtech <abid.khan@nashtechglobal.com>
2023-10-06 12:58:42 +0530

    removed unused repo

--- cart-service/src/main/java/com/nashtech/car/cart/repository/ProductsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nashtech.car.cart.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductsRepository extends JpaRepository<ProductEntity, String> {
-	ProductEntity findByProductId(String productId);
-
-}

b1af8d59e52bea1da8056ae74e87e21398f9fd76
abidknashtech <abid.khan@nashtechglobal.com>
2023-11-13 17:00:29 +0530

    correct GCP key

--- deployment/gcpresources/terraform/my-sql-instance-variables.tf
+++ deployment/gcpresources/terraform/my-sql-instance-variables.tf
@@ -70,8 +70,3 @@ variable user_password {
   description = "The password for the default user. If not set, a random one will be generated and available in the generated_user_password output variable."
   default     = "JavaDBs#password2023"
 }
-
-variable gcp_json_key {
-  description = "GCP Json key"
-  default     = "{}"
-}

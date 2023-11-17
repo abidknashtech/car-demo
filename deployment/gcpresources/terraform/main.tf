69e411cff465e0e8a684f781eccaa296323fc4c7
abidknashtech <127501072+abidknashtech@users.noreply.github.com>
2023-11-17 15:48:58 +0530

    Update main.tf

--- deployment/gcpresources/terraform/main.tf
+++ deployment/gcpresources/terraform/main.tf
@@ -100,6 +100,24 @@ resource "google_pubsub_subscription" "inventory_subscription" {
   enable_message_ordering    = false
 }
 
+resource "google_pubsub_subscription" "shipment_subscription" {
+  name  = "shipment_subscription"
+  topic = google_pubsub_topic.shipment-notification.name
+
+  # 20 minutes
+  message_retention_duration = "1200s"
+  retain_acked_messages      = true
+
+  ack_deadline_seconds = 20
+  expiration_policy {
+    ttl = "300000.5s"
+  }
+  retry_policy {
+    minimum_backoff = "10s"
+  }
+  enable_message_ordering    = false
+}
+
 #-----------------------GKE Cluster for applications----------------------------
 resource "google_container_cluster" "car-demo-gke" {
   name     = "car-demo-gke"

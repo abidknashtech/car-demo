c42965230e9138da70ee9f8e13a28bc5c54117fc
abidknashtech <abid.khan@nashtechglobal.com>
2023-12-05 16:42:18 +0530

    added order-status endpoint

--- order-service/src/main/java/com.nashtech.order/query/OrderQueriesHandler.java
+++ order-service/src/main/java/com.nashtech.order/query/OrderQueriesHandler.java
@@ -1,26 +1,23 @@
 package com.nashtech.order.query;
 
-import com.nashtech.common.utils.OrderStatus;
 import com.nashtech.order.repository.OrderRepository;
-import com.nashtech.order.restapi.response.OrderSummary;
+import com.nashtech.order.repository.entity.OrderEntity;
 import org.axonframework.queryhandling.QueryHandler;
 import org.springframework.stereotype.Component;
+import java.util.List;
 
 @Component
 public class OrderQueriesHandler {
 
-    OrderRepository ordersRepository;
+    private final OrderRepository ordersRepository;
 
     public OrderQueriesHandler(OrderRepository ordersRepository) {
         this.ordersRepository = ordersRepository;
     }
 
     @QueryHandler
-    public OrderSummary findOrder(FindOrderQuery findOrderQuery) {
-        return OrderSummary.builder()
-                .orderId(findOrderQuery.getOrderId())
-                .orderStatus(OrderStatus.ORDER_PLACED.toString())
-                .build();
+    public List<OrderEntity> findOrders(FindOrdersByUserQuery findOrdersByUserQuery) {
+        return ordersRepository.findByUserId(findOrdersByUserQuery.getUserId());
     }
 
 }

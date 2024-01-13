916d631165b15f98adf40bf39075d009a20930b4
shivamm31 <shivam.roy@knoldus.com>
2024-01-13 14:31:27 +0530

    added few test cases for handler

--- inventory-service/src/test/java/com/nashtech/inventory/handler/ProductEventsHandlerTest.java
+++ inventory-service/src/test/java/com/nashtech/inventory/handler/ProductEventsHandlerTest.java
@@ -1,8 +1,5 @@
 package com.nashtech.inventory.handler;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 import com.nashtech.common.event.ProductReserveCancelledEvent;
 import com.nashtech.common.event.ProductReservedEvent;
 import com.nashtech.inventory.events.ProductCreatedEvent;
@@ -21,6 +18,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.mockito.Mockito.*;
 
 @ContextConfiguration(classes = {ProductEventsHandler.class})
 @ExtendWith(SpringExtension.class)
@@ -170,5 +169,98 @@ class ProductEventsHandlerTest {
         // Assert
         verify(productsRepository).save(Mockito.<ProductEntity>any());
     }
+    @Test
+    void testOn4() {
+        // Arrange
+        ProductEntity productEntity = new ProductEntity();
+        productEntity.setBasePrice(10.0d);
+        productEntity.setBrand("Brand");
+        productEntity.setColor("Color");
+        productEntity.setMileage(10.0d);
+        productEntity.setModel("Model");
+        productEntity.setProductId("42");
+        productEntity.setQuantity(1);
+        productEntity.setTax(10.0f);
+        productEntity.setTimestamp(Date.from(LocalDate.of(1970, 1, 1).atStartOfDay().atZone(ZoneOffset.UTC).toInstant()));
+        productEntity.setYear(1);
 
+        ProductEntity productEntity2 = new ProductEntity();
+        productEntity2.setBasePrice(10.0d);
+        productEntity2.setBrand("Brand");
+        productEntity2.setColor("Color");
+        productEntity2.setMileage(10.0d);
+        productEntity2.setModel("Model");
+        productEntity2.setProductId("42");
+        productEntity2.setQuantity(1);
+        productEntity2.setTax(10.0f);
+        productEntity2.setTimestamp(Date.from(LocalDate.of(1970, 1, 1).atStartOfDay().atZone(ZoneOffset.UTC).toInstant()));
+        productEntity2.setYear(1);
+        when(productsRepository.save(Mockito.<ProductEntity>any())).thenReturn(productEntity2);
+        when(productsRepository.findByProductId(Mockito.<String>any())).thenReturn(productEntity);
+        ProductReserveCancelledEvent productReservationCancelledEvent = ProductReserveCancelledEvent.builder()
+                .orderId("42")
+                .productId("42")
+                .quantity(1)
+                .userId("42")
+                .build();
+
+        // Act
+        productEventsHandler.on(productReservationCancelledEvent);
+
+        // Assert
+        verify(productsRepository).findByProductId(Mockito.<String>any());
+        verify(productsRepository).save(Mockito.<ProductEntity>any());
+    }
+    @Test
+    void testProductReservedEventHandling() {
+        // Arrange
+        ProductEntity productEntity = new ProductEntity();
+        productEntity.setBasePrice(10.0d);
+        productEntity.setBrand("Brand");
+        productEntity.setColor("Color");
+        productEntity.setMileage(10.0d);
+        productEntity.setModel("Model");
+        productEntity.setProductId("42");
+        productEntity.setQuantity(1);
+        productEntity.setTax(10.0f);
+        productEntity.setTimestamp(Date.from(LocalDate.of(1970, 1, 1).atStartOfDay().atZone(ZoneOffset.UTC).toInstant()));
+        productEntity.setYear(1);
+
+        ProductEntity productEntity2 = new ProductEntity();
+        productEntity2.setBasePrice(10.0d);
+        productEntity2.setBrand("Brand");
+        productEntity2.setColor("Color");
+        productEntity2.setMileage(10.0d);
+        productEntity2.setModel("Model");
+        productEntity2.setProductId("42");
+        productEntity2.setQuantity(1);
+        productEntity2.setTax(10.0f);
+        productEntity2.setTimestamp(Date.from(LocalDate.of(1970, 1, 1).atStartOfDay().atZone(ZoneOffset.UTC).toInstant()));
+        productEntity2.setYear(1);
+        when(productsRepository.save(Mockito.<ProductEntity>any())).thenReturn(productEntity2);
+        when(productsRepository.findByProductId(Mockito.<String>any())).thenReturn(productEntity);
+        ProductReservedEvent productReservedEvent = ProductReservedEvent.builder()
+                .basePrice(10.0d)
+                .brand("Brand")
+                .color("Color")
+                .mileage(10.0d)
+                .model("Model")
+                .orderId("42")
+                .productId("42")
+                .quantity(1)
+                .subTotal(10.0d)
+                .tax(10.0f)
+                .total(10.0d)
+                .totalTax(10.0f)
+                .userId("42")
+                .year(1)
+                .build();
+
+        // Act
+        productEventsHandler.on(productReservedEvent);
+
+        // Assert
+        verify(productsRepository).findByProductId(Mockito.<String>any());
+        verify(productsRepository).save(Mockito.<ProductEntity>any());
+    }
 }

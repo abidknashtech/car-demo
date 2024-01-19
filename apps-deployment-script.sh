bc83575b1c15918f9e10a0d0a36bbc8c78a46ae5
abidknashtech <127501072+abidknashtech@users.noreply.github.com>
2024-01-19 14:15:32 +0530

    Update apps-deployment-script.sh

--- apps-deployment-script.sh
+++ apps-deployment-script.sh
@@ -14,7 +14,7 @@ build_and_deploy_service(){
    echo "---------build and deploy $SERVICE_NAME-----------"
    cd "$SERVICE_NAME" || exit
    if [  $SERVICE_NAME != "car-ui" ]; then
-       mvn verify sonar:sonar
+      # mvn verify sonar:sonar
        mvn clean install  -s $GITHUB_WORKSPACE/settings.xml
    fi
    echo "---------packaging done, start docker build-----------"
